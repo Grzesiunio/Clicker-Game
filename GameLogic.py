b787ba3b01d7f26b9030b3d32edc36ceb8c3c69b
Grzesiunio <48492676+Grzesiunio@users.noreply.github.com>
2020-07-10 15:28:42 +0200

    Add files via upload

--- GameLogic.py
+++ GameLogic.py
@@ -20,9 +20,9 @@ pygame.display.set_caption("Clicker Game")
 gold = 0
 
 # Creating buttons
-hotel_button = pygame.Rect(200, 120, 50, 50)
-motel_button = pygame.Rect(200, 190, 50, 50)
-house_button = pygame.Rect(200, 260, 50, 50)
+hotel_button = pygame.Rect(300, 120, 50, 50)
+motel_button = pygame.Rect(300, 190, 50, 50)
+house_button = pygame.Rect(300, 260, 50, 50)
 # Keys
 space_down = False
 mouse_click_down = False
@@ -35,6 +35,9 @@ myfont = pygame.font.SysFont('Comic Sans MS', 30)
 hotel = Hotel(0, 0)
 motel = Motel(0,0)
 house = House(0,0)
+
+# Stopwatch
+start = time.time()
 
 # Game Loop
 running = True
@@ -55,8 +58,8 @@ while running:
 
     # hotel button
     if event.type == pygame.MOUSEBUTTONDOWN:
-        if pygame.mouse.get_pos()[0] >= 200 and pygame.mouse.get_pos()[1] >= 120:
-            if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 170:
+        if pygame.mouse.get_pos()[0] >= 300 and pygame.mouse.get_pos()[1] >= 120:
+            if pygame.mouse.get_pos()[0] <= 350 and pygame.mouse.get_pos()[1] <= 170:
                 pygame.time.delay(120)
                 hotel.hotel_buy(gold)
                 if gold >= 20:
@@ -64,8 +67,8 @@ while running:
                     gold -= 20
     # motel button
     if event.type == pygame.MOUSEBUTTONDOWN:
-        if pygame.mouse.get_pos()[0] >= 200 and pygame.mouse.get_pos()[1] >= 190:
-            if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 260:
+        if pygame.mouse.get_pos()[0] >= 300 and pygame.mouse.get_pos()[1] >= 190:
+            if pygame.mouse.get_pos()[0] <= 350 and pygame.mouse.get_pos()[1] <= 260:
                 pygame.time.delay(120)
                 motel.motel_buy(gold)
                 if gold >= 1000:
@@ -74,8 +77,8 @@ while running:
 
     # House button
     if event.type == pygame.MOUSEBUTTONDOWN:
-        if pygame.mouse.get_pos()[0] >= 200 and pygame.mouse.get_pos()[1] >= 260:
-            if pygame.mouse.get_pos()[0] <= 250 and pygame.mouse.get_pos()[1] <= 310:
+        if pygame.mouse.get_pos()[0] >= 300 and pygame.mouse.get_pos()[1] >= 260:
+            if pygame.mouse.get_pos()[0] <= 350 and pygame.mouse.get_pos()[1] <= 310:
                 pygame.time.delay(120)
                 house.house_buy(gold)
                 if gold >= 100000:
@@ -85,11 +88,20 @@ while running:
     if int(time.time() % 30) == 1:
         pygame.time.delay(100)
         gold = gold + hotel.quantity
+    # Motel
+    if int(time.time() % 20) == 1:
+        pygame.time.delay(90)
+        gold = gold + 2*hotel.quantity
+    # House
+    if int(time.time() % 10) == 1:
+        pygame.time.delay(80)
+        gold = gold + 3*house.quantity
     # Text with gold
     textsurface = myfont.render('Your Gold: ' + str(gold), False, (0, 0, 0))
-    text_hotel = myfont.render('Quantity: ' + str(hotel.quantity), False, (0,0,0))
-    text_motel = myfont.render('Quantity: ' + str(motel.quantity), False, (0,0,0))
-    text_house = myfont.render('Quantity: ' + str(house.quantity), False, (0,0,0))
+    text_hotel = myfont.render('Hotel quantity: ' + str(hotel.quantity), False, (0,0,0))
+    text_motel = myfont.render('Motel quantity: ' + str(motel.quantity), False, (0,0,0))
+    text_house = myfont.render('House quantity: ' + str(house.quantity), False, (0,0,0))
+    text_stopwatch = myfont.render('Time: ' +str(int(time.time()-start)) +'s', False, (0,0,0))
 
     # Color screen
     screen.fill((0, 255, 0))
@@ -101,6 +113,7 @@ while running:
     screen.blit(text_hotel, (10, 120))
     screen.blit(text_motel, (10, 190))
     screen.blit(text_house, (10, 260))
+    screen.blit(text_stopwatch, (620,0))
     pygame.display.update()
 
 # TODO:
